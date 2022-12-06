db8d88fbddce740e8939e58c633b8be282cfd320
hexum439 <96937556+hexum439@users.noreply.github.com>
2022-12-06 01:24:15 +0100

    Add project files.

---
+++ Controllers/OblacilasController.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SecondHand.Data;
+
+namespace SecondHand.Controllers
+{
+    public class OblacilasController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public OblacilasController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Oblacilas.ToList();
+            return View(data);
+        }
+    }
+}
---
+++ Controllers/ObutevsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SecondHand.Data;
+
+namespace SecondHand.Controllers
+{
+    public class ObutevsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ObutevsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var allCevlji = _context.Obutevs.ToList();
+            return View();
+        }
+    }
+}
